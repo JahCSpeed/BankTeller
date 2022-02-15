d78ab82f00cb92a1c2217cb21ab2ec577c97e16f
BLIGHT <abevit2000@gmail.com>
2022-02-15 18:27:44 -0500

    BankTeller initial framework + enum class

--- bankTeller/BankTeller.java
+++ bankTeller/BankTeller.java
@@ -1,7 +1,73 @@
 package bankTeller;
 
-public class BankTeller {
+import java.util.Scanner;
 
+import accounts.AccountDatabase;
+
+public class BankTeller {
+	private boolean isRunning;
+	private Scanner scanInput;
+	private AccountDatabase mainDatabase;
+	
+	/**
+	 Constructs and initializes the variables to enable the BankTeller to run.
+	 */
+	public BankTeller() {
+		this.isRunning = false;
+		this.scanInput = new Scanner(System.in);
+		this.mainDatabase = new AccountDatabase();
+	}
+	
+	public void run() {
+		//Runs the Bank Teller
+	}
+	
+	private void endBankTeller() {
+		System.out.println("Bank Teller is terminated.");
+		this.isRunning = !this.isRunning;
+	}
+	
+	private int doNameCommand(int command) {
+		switch(command) {
+			case 1:
+				//O command
+			case 2:
+				//C command
+			case 3:
+				//D command
+			case 4:
+				//W command
+			default:
+				return -1;
+		}
+	}
+	
+	private int doNoNameCommand(int command) {
+		switch(command) {
+			case 5:
+				//P command
+			case 6:
+				//PT command
+			case 7:
+				//PI command
+			case 8:
+				//UB command
+			case 9:
+				//Q command
+			default:
+				return -1;
+		}
+	}
+	
+	/**
+	 Checks if the person already has an account in
+	 @param person
+	 @return
+	 */
+	private boolean checkProfile(Profile person) {
+		
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
---
+++ bankTeller/Commands.java
@@ -0,0 +1,28 @@
+package bankTeller;
+
+/**
+ The commands class holds all the valid command information for the BankTeller to use.
+ It lists all the valid commands as ids for use in the BankTeller, where further
+ actions will be done depending on the command.
+ @author Abe Vitangcol
+ */
+public enum Commands {
+	O(1),
+	C(2),
+	D(3),
+	W(4),
+	P(5),
+	PT(6),
+	PI(7),
+	UB(8),
+	Q(9);
+	public final int id;
+	
+	/**
+	 Constructs the identification code for each command.
+	 @param id The identification code for the given command.
+	 */
+	private Commands(int id) {
+		this.id = id;
+	}
+}
